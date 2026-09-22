7b343c4b3c35251eb5290f3e56b9732bd30f10e4
HieuDN <duonghieu294@gmail.com>
2026-09-22 08:11:05 +0700

    fix: build frontend

--- class-frontend/src/features/teaching/SessionOperationsPage.tsx
+++ class-frontend/src/features/teaching/SessionOperationsPage.tsx
@@ -47,7 +47,6 @@ import { PageSkeleton } from "../../shared/ui/Skeleton";
 import { StatePanel } from "../../shared/ui/StatePanel";
 import { useToast } from "../../shared/ui/Toast";
 import { HomeworkCreateModal } from "../content/HomeworkCreateModal";
-import { SessionMutationModal } from "../schedules/components/SessionMutationModal";
 import { CompletionCorrectionModal } from "./CompletionCorrectionModal";
 
 const optionalUrl = z
